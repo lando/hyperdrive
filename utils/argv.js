5b0e074f36d25b295912e1cc76141eea01a9e0cf
John Ouellet <john@thinktandem.io>
2021-09-27 14:38:05 -0400

    Fix tests

--- utils/argv.js
+++ utils/argv.js
@@ -1,25 +1,24 @@
 
-const hasOption = (option) => {
+const hasOption = option => {
   return process.argv.slice(2).some(element => element.includes(option));
-}
- 
+};
+
 const getOption = (option, args) => {
-  const defaultValue = args.hasOwnProperty('defaultValue') ? args.defaultValue : '';
+  const defaultValue = Object.prototype.hasOwnProperty.call(args, 'defaultValue') ? args.defaultValue : '';
   const optionIndex = process.argv.indexOf(option);
 
   let optionValue;
   if (option.indexOf('=') >= 0) {
     optionValue = option.split('=')[1];
-  }
-  else {
+  } else {
     const checkNext = process.argv[optionIndex + 1];
     optionValue = checkNext === undefined || checkNext.indexOf('--') >= 0 ? defaultValue : checkNext;
   }
 
   return optionValue;
-}
- 
+};
+
 module.exports = {
-  hasOption, 
+  hasOption,
   getOption,
 };
