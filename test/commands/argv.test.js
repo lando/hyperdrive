c4b9de6028ccb44f547905445eaf754d19bc429d
John Ouellet <john@thinktandem.io>
2021-09-30 16:12:10 -0400

    #29 fix up tests and add in doc blocks

--- test/commands/argv.test.js
+++ test/commands/argv.test.js
@@ -5,143 +5,147 @@ chai.should();
 
 describe('argv', () => {
   describe('hasOption', () => {
-    beforeEach(() => {
+    it('should return true if option exists in argv', () => {
       process.argv = [
         'node',
         'hyperdrive',
         'list',
         '--debug',
       ];
-    });
-
-    it('should return true if option exists in argv', () => {
       const hasOption = argv.hasOption('--debug');
       hasOption.should.equal(true);
     });
 
     it('should return false if option does not exist in argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+      ];
       const hasOption = argv.hasOption('--trill');
       hasOption.should.equal(false);
     });
   });
 
   describe('getOption', () => {
-    describe('wildcard', () => {
-      beforeEach(() => {
-        process.argv = [
-          'node',
-          'hyperdrive',
-          'list',
-          '--debug',
-        ];
-      });
-
-      it('should return default flag if option is boolean and matches argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.equal('*');
-      });
-
-      it('should return true if option is boolean, matches argv, and default value is not set', () => {
-        const getOption = argv.getOption('--debug');
-        getOption.should.equal(true);
-      });
-
-      it('should not return default flag if option is boolean and does not match argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.not.equal('trill');
-      });
+    it('should return default flag if option is boolean and matches argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.equal('*');
     });
 
-    describe('namespace with equal sign', () => {
-      beforeEach(() => {
-        process.argv = [
-          'node',
-          'hyperdrive',
-          'list',
-          '--debug="trill"',
-        ];
-      });
-
-      it('should match string if option is string with equal sign and flag matches argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.equal('"trill"');
-      });
-
-      it('should not match string if option is string with equal sign and flag does not matche argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.not.equal('"tronic"');
-      });
+    it('should return true if option is boolean, matches argv, and default value is not set', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+      ];
+      const getOption = argv.getOption('--debug');
+      getOption.should.equal(true);
     });
 
-    describe('namespace with space', () => {
-      beforeEach(() => {
-        process.argv = [
-          'node',
-          'hyperdrive',
-          'list',
-          '--debug',
-          '"trill"',
-        ];
-      });
-
-      it('should match string if option is string with space after flag and flag matches argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.equal('"trill"');
-      });
-
-      it('should not match string if option is string with space after flag and flag does not match argv', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.not.equal('"tronic"');
-      });
+    it('should not return default flag if option is boolean and does not match argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.not.equal('trill');
     });
 
-    describe('wildcard with multiple flags', () => {
-      beforeEach(() => {
-        process.argv = [
-          'node',
-          'hyperdrive',
-          'list',
-          '--debug',
-          '--help',
-        ];
-      });
-
-      it('should return default flag if option is boolean, matches argv, and multiple flags are present', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.equal('*');
-      });
-
-      it('should not return next flag if option is boolean, matches argv, and multiple flags are present', () => {
-        const getOption = argv.getOption('--debug', {defaultValue: '*'});
-        getOption.should.not.equal('--help');
-      });
+    it('should match string if option is string with equal sign and flag matches argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug="trill"',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.equal('"trill"');
     });
 
-    describe('flag is null', () => {
-      beforeEach(() => {
-        process.argv = [
-          'node',
-          'hyperdrive',
-          'list',
-          '--debug',
-        ];
-      });
+    it('should not match string if option is string with equal sign and flag does not matche argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug="trill"',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.not.equal('"tronic"');
+    });
 
-      it('should throw an error as flag is set incorrectly', () => {
-        expect(() => argv.getOption('--trill', {defaultValue: '*'})).to.throw(Error); // eslint-disable-line max-nested-callbacks
-      });
+    it('should match string if option is string with space after flag and flag matches argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+        '"trill"',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.equal('"trill"');
+    });
 
-      it('should throw an error as flag is empty', () => {
-        expect(() => argv.getOption('')).to.throw(Error); // eslint-disable-line max-nested-callbacks
-      });
+    it('should not match string if option is string with space after flag and flag does not match argv', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+        '"trill"',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.not.equal('"tronic"');
+    });
 
-      it('should throw an error as flag is undefined', () => {
-        expect(() => argv.getOption()).to.throw(Error); // eslint-disable-line max-nested-callbacks
-      });
+    it('should return default flag if option is boolean, matches argv, and multiple flags are present', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+        '--help',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.equal('*');
+    });
 
-      it('should throw an error as flag is null', () => {
-        expect(() => argv.getOption(null)).to.throw(Error); // eslint-disable-line max-nested-callbacks
-      });
+    it('should not return next flag if option is boolean, matches argv, and multiple flags are present', () => {
+      process.argv = [
+        'node',
+        'hyperdrive',
+        'list',
+        '--debug',
+        '--help',
+      ];
+      const getOption = argv.getOption('--debug', {defaultValue: '*'});
+      getOption.should.not.equal('--help');
+    });
+
+    it('should throw an error as flag is set incorrectly', () => {
+      expect(() => argv.getOption('--trill', {defaultValue: '*'})).to.throw(Error); // eslint-disable-line max-nested-callbacks
+    });
+
+    it('should throw an error as flag is empty', () => {
+      expect(() => argv.getOption('')).to.throw(Error); // eslint-disable-line max-nested-callbacks
+    });
+
+    it('should throw an error as flag is undefined', () => {
+      expect(() => argv.getOption()).to.throw(Error); // eslint-disable-line max-nested-callbacks
+    });
+
+    it('should throw an error as flag is null', () => {
+      expect(() => argv.getOption(null)).to.throw(Error); // eslint-disable-line max-nested-callbacks
     });
   });
 });
--- utils/argv.js
+++ utils/argv.js
@@ -1,16 +1,19 @@
-
+// Gets the current argv element index.
 const getCurrentElementIndex = flag => {
   return process.argv.findIndex(element => element.indexOf(flag) >= 0);
 };
 
+// Gets the current argv element value.
 const getCurrentElement = flag => {
   return process.argv[getCurrentElementIndex(flag)];
 };
 
+// Gets the next argv element value.
 const getNextElement = flag => {
   return process.argv[getCurrentElementIndex(flag) + 1];
 };
 
+// Checks the flag type.
 const getFlagType = flag => {
   // If the current element has an = then immediately return a string.
   if (getCurrentElement(flag).includes('=')) return 'string';
@@ -19,30 +22,30 @@ const getFlagType = flag => {
   return getNextElement(flag) === undefined ||  getNextElement(flag).startsWith('-') ? 'boolean' : 'string';
 };
 
-// stuff
+// Gets the flag string value.
 const getStringValue = flag => {
   return getCurrentElement(flag).split('=')[1] ?? getNextElement(flag);
 };
 
 /**
+ * Checks if argv has the defined flag.
  *
- * @param {*} flag
- * @returns
+ * @param {string} flag The flag we are checking against argv.
+ * @returns {boolean} If the flag is present or not.
  */
 const hasOption = flag => {
   return process.argv.slice(2).some(element => element.split('=')[0] === flag);
 };
 
 /**
+ * Gets the defined flag string, boolean, or default value.
  *
- * @param {*} flag
- * @param {*} options
- * @returns
+ * @param {string} flag The flag value we are getting.
+ * @param {object} options An object of options.
+ * @returns {string|boolean} The value of the flag.
  */
 const getOption = (flag, options = {}) => {
-  // // Immediately fail if flag is undefined, null, or empty.
-  // if (flag === undefined || flag === null || flag === '') throw new Error('Flag is not set');
-
+  // Immediately fail if flag is not correct, undefined, null, or empty.
   if (process.argv.slice(2).some(element => element.split('=')[0] === flag) === false) throw new Error('No such flag');
 
   // If flag is boolean then return default value or true
