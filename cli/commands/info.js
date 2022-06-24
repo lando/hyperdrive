12caf063a8bb72ec0bdbf80e36a9590094b7f1e0
Alec Reynolds <alec+git@lando.dev>
2022-06-24 12:28:19 -0700

    Update notes on hyperdrive info architecture.

--- cli/commands/info.js
+++ cli/commands/info.js
@@ -39,17 +39,7 @@ class InfoCommand extends BaseCommand {
     namespace: Flags.string({
       char: 'n',
       description: 'Show installed plugins in the specified namespace context (defaults used for projects using the namespace).'
-    }),
-    registry: Flags.string({
-      char: 'r',
-      description: 'NPM registry to fetch plugins from.',
-      default: 'https://registry.npmjs.org',
-    }),
-    auth: Flags.string({
-      char: 'a',
-      description: 'Token to use for authentication to the specified NPM registry.',
-      default: null,
-    }),
+    })
   };
 
   // static args
@@ -64,17 +54,13 @@ class InfoCommand extends BaseCommand {
     const query = `/${args.plugin}`;
     this.log(flags);
     const opts = {
-      registry: flags.registry,
-      '//npm.pkg.github.com/:_authToken': flags.auth,
-      // Integrate config file npm section that allows you to defined npmrc options to pass in to here/other npm-related commands.
+      // Integrate config file npm section that allows you to define npmrc options to pass in to here/other npm-related commands.
+
+      //This is the format required for authing with an authtoken...maybe put this in a demo config file.
+      '//<npm.pkg.github.com>/:_authToken': 'THE AUTH TOKEN',
     }
     const info = await npmFetch.json(query, opts)
     this.log(info);
-
-
-
-    // How do we get info from another registry? Authentication? Option to specify access token? Way to reference the auth and registry from a .npmrc?
-    // hyperdrive info @foxcorp/foxkit-lib-common-js --auth= --registry=https://npm.pkg.github.com
   }
 }
 
