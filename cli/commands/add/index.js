8df47e5be805a6c2c8a34dd628f7893c0618c6f1
Alec Reynolds <alec+git@lando.dev>
2022-07-05 12:06:54 -0700

    Add command (#42)
    * Prototyping the add command for fun and profit.
    
    * Handle namespaced and non-namespaced plugins in add command.
    
    * hyperdrive add: create namespace on the host before plugin install.
    
    * hyperdrive add: allow multiple plugins, debugging output stream.
    
    * hyperdrive add: improve debugging output for multiple installs. Use cp instead of mv.
    
    * hyperdrive add: completely replace previously downloaded plugin.
    
    * hyperdrive add lando: basic download of static asset with progress bar.
    
    * Feeding the linter.

--- cli/commands/add/index.js
+++ cli/commands/add/index.js
@@ -1,5 +1,6 @@
 const {PluginCommand} = require('../../lib/plugin-command');
-const {flags} = require('@oclif/core');
+const {CliUx} = require('@oclif/core');
+const path = require('path');
 
 class AddCommand extends PluginCommand {
   // @TODO: For individual apps, you can create a .npmrc file to specify private registry.
@@ -7,25 +8,79 @@ class AddCommand extends PluginCommand {
   // For global context, we should have a global config option for specifying a .npmrc file OR allow the user to put .npmrc in your Hyperdrive config. Need to figure out precedence.
   static description = 'Add a plugin or dependency to your current Lando context.';
   static usage = 'usage';
+  static args = [
+    ...PluginCommand.args,
+  ];
+
+  static flags = {
+    ...PluginCommand.flags,
+  };
 
   static aliases = ['install'];
 
   static examples = [
-    'hyperdrive add apache --global',
-    'hyperdrive add apache@0.5.0'
+    'hyperdrive add @lando/apache --global',
+    'hyperdrive add @lando/apache@0.5.0',
   ];
 
+  static strict = false;
+
   async run() {
-    const {Hyperdrive} = require('../../../lib/hyperdrive');
-    const hyperdrive = new Hyperdrive();
-    // Check the args; if it's a "protected" dependency like Lando or Docker Desktop, then
-    // run the custom installation for those.
+    const {execa} = await import('execa');
+    const utils = require('../../lib/utils');
+    const mkdirp = require('mkdirp');
+    const minimist = require('minimist');
+    const fs = require('fs');
 
     // Lando should install Docker Desktop by default, but have a flag --no-docker-desktop that would skip installing it.
     // OCLIF "Topics" to create a subcommand `hyperdrive add lando`/`hyperdrive add docker-desktop`, which may be useful for creating these distinct variations for Lando/Docker Desktop
-    const {flags, args} = this.parse(AddCommand);
-    // @todo: should we parse the version here or in hyperdrive.js?
-    if (flags.global) hyperdrive.addPlugin(args.plugin);
+    const {flags, argv} = await this.parse(AddCommand);
+    const fargv = minimist(argv)._;
+
+    // Start the spinner
+    CliUx.ux.action.start('Installing...');
+
+    // Move the scripts folder into the OCLIF data directory.
+    const scripts = path.join(this.config.dataDir, 'scripts');
+    const home = this.config.home;
+    utils.moveConfig(path.resolve(__dirname, '..', '..', '..', 'scripts'), scripts);
+
+    // @todo: context detection. If we're in a Lando app, we'll add the plugin
+    // to that app. We'll need to create a @lando/bootstrap package that will
+    // provide the functionality to...
+    //  - analyze/load Lando global config
+    //  - load a Lando app
+    // ...in our case this will be used to detect context and load the Landofile.
+
+    // App install logic.
+
+    // Namespace install logic.
+
+    // Global install logic.
+    if (flags.global) {
+      // Run docker commands to install plugins.
+      try {
+        await utils.map(fargv, function(plugin) {
+          const pluginFolder = '/' + plugin;
+          const pluginFolderPath = `${home}/.lando/plugins${pluginFolder}`;
+          if (fs.existsSync(pluginFolderPath)) {
+            fs.rmSync(pluginFolderPath, {recursive: true});
+          }
+
+          mkdirp.sync(pluginFolderPath);
+          const subprocess = execa('docker', ['run', '--rm', '-v', `${home}/.lando/plugins${pluginFolder}:/plugins${pluginFolder}`, '-v', `${scripts}:/scripts`, '-w', '/tmp', 'node:14-alpine', 'sh', '-c', `/scripts/add.sh ${plugin}`]);
+          subprocess.stdout.on('data', buffer => {
+            const debug = require('debug')(`add-${plugin}:@lando/hyperdrive`);
+            debug(String(buffer));
+          });
+          return subprocess;
+        });
+        CliUx.ux.action.stop();
+      } catch (error) {
+        // @TODO: Some sort of nice error message? What can we cull?
+        this.error(error);
+      }
+    }
   }
 }
 
