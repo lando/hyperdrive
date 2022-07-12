201d172ab1ef14fc11baa7972ece177a0a51eec6
Mike Pirog <mike@lando.dev>
2022-07-12 09:49:30 -0400

    mix in some other helpful config eg packaged status

--- cli/hooks/minstrap.js
+++ cli/hooks/minstrap.js
@@ -1,5 +1,19 @@
 const debug = require('debug')('minstrap:@lando/hyperdrive');
+const has = require('lodash/has');
 
-module.exports = async() => {
+module.exports = async({config}) => {
   debug('minstrap event works!');
+
+  // mix in some additional and helpful config
+  // the environment we are running in
+  config.env = has(process, 'pkg') ? 'prod' : 'dev';
+  // is running in a leia test
+  config.leia = has(process, 'env.LEIA_PARSER_RUNNING');
+  // legacy
+  config.mode = 'cli';
+  // is running from a binary packaged up by @vercel/pkg
+  config.packaged = has(process, 'pkg');
+  // just some other identifiers
+  config.id = 'hyperdrive';
+  config.product = 'hyperdrive';
 };
