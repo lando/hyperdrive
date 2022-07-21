6934a6853b35dc2f0890c130f5d7fbc9d089d49a
Mike Pirog <mike@lando.dev>
2022-07-21 11:15:50 -0400

    basic lando-cli class
    * basics of lando class
    
    * basics of lando class part 2

--- cli/commands/list.js
+++ cli/commands/list.js
@@ -1,7 +1,6 @@
 const {BaseCommand} = require('../lib/base-command');
 const {CliUx, Flags} = require('@oclif/core');
-const Plugin = require('./../../core/plugin');
-const Lando = require('./../../core/lando');
+const LandoCLI = require('../../core/lando-cli');
 
 class ListCommand extends BaseCommand {
   static description = 'gets plugins for given context';
@@ -21,34 +20,22 @@ class ListCommand extends BaseCommand {
   };
 
   async run() {
-    // load slower modules
-    // @TODO: remove lodash in favor of mostly native
-    const _ = require('lodash');
+    // modes
+    const sortBy = require('lodash/sortBy');
     // get args and flags
     const {flags} = await this.parse(ListCommand);
     // get helpers
     const config = this.config.hyperdrive;
-    const {findPlugins, sortPlugins} = this.config.Bootstrapper;
-    const lando = new Lando({...config.get('core'), ...config.get('lando')});
+    const lando = new LandoCLI({...config.get('core'), ...config.get('lando')});
 
-    // @TODO: if lando is installed then get its config
-    this.debug('acquired lando configuration %o', lando.config);
+    // @TODO: if lando is not installed or is unsupported then throw an error?
+    if (!lando.isInstalled || !lando.isSupported) {
+      this.error('make this good later!');
+    }
 
-    // scan any lando provided global directories for additional plugins
-    const globalPluginDirs = lando.config.pluginDirs
-    .filter((dir => dir.type === 'global'))
-    .map(dir => findPlugins(dir.dir, dir.depth))
-    .flat(Number.POSITIVE_INFINITY);
-    this.debug('found additional globally installed plugins in %o', globalPluginDirs);
-
-    // iterate through globalPlugins and instantiate them
-    // grab our plugin config
-    // @NOTE: format and settings TBH still
-    const channel = this.config.hyperdrive.get('core.release-channel');
-    const pluginConfig = {channel, ...this.config.hyperdrive.get('plugins')};
-    const globalPlugins = globalPluginDirs
-    .map(dir => new Plugin(dir, pluginConfig))
-    .map(plugin => ({...plugin, type: 'global'}));
+    // start by getting lando provided plugins
+    const plugins = lando.getPlugins();
+    this.debug('acquired lando provided plugins %o', plugins.map(plugin => `${plugin.name}@${plugin.version}`));
 
     // determine app context or not, bootsrtrap method to load in complete landofile?
     // if app context then load in the landofiles using a bootstrap method?
@@ -56,15 +43,8 @@ class ListCommand extends BaseCommand {
     // the landofile config?
     // merge in app plugin stuff?
 
-    // concat our plugins together, sort them and make them ready for display
-    const plugins = _(sortPlugins([...lando.config.plugins, ...globalPlugins]))
-    .map((plugins, name) => ({...plugins[0], name}))
-    .map(plugin => _.pick(plugin, ['name', 'package', 'type', 'location', 'version', 'isValid', 'isHidden', 'deprecated']))
-    .sortBy('name')
-    .value();
-
     // filter out invalid and hidden plugins
-    const rows = plugins.filter(plugin => plugin.isValid && !plugin.isHidden);
+    const rows = sortBy(plugins.filter(plugin => plugin.isValid && !plugin.isHidden), 'name');
 
     // if JSON then return here
     if (flags.json) return rows;
