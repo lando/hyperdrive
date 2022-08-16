5829a903d60d7d247222141f83c6084794b24db6
Alec Reynolds <alec+git@lando.dev>
2022-08-16 11:55:31 -0700

    hyperdrive add: accept npmrc options for Docker Desktop.

--- scripts/docker-desktop/plugin-add.sh
+++ scripts/docker-desktop/plugin-add.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yarn init -y
-yarn add $1 --no-progress --production --flat --no-default-rc --no-lockfile --link-duplicates
-yarn install --no-progress --production --cwd /tmp/node_modules/$2
+npm init -y
+npm add $1 --no-progress --production --flat --no-default-rc --no-lockfile --link-duplicates
+npm install --no-progress --production -C /tmp/node_modules/$2
 cp -rf /tmp/node_modules/$2/* /plugins/$2
